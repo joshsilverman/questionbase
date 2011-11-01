0877052e274d530c3de50eab29c4d610204753d8
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-11-01 11:02:11 -0400

    migration sets default score for old records

--- db/migrate/20111031061752_add_score_to_questions.rb
+++ db/migrate/20111031061752_add_score_to_questions.rb
@@ -1,6 +1,11 @@
 class AddScoreToQuestions < ActiveRecord::Migration
   def self.up
     add_column :questions, :score, :integer, :default => 1600
+    
+    questions = Question.all
+    questions.each do |q|
+      q.update_attribute(:score, 1600)
+    end
   end
 
   def self.down
